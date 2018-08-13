2c3e990495d8bcc6eae586de5a5aa6ee846bdf9b
TOH ENG JIE <16019641@rp.edu.sg>
2018-08-13 16:48:10 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/senderapp/MainActivity.java
@@ -0,0 +1,33 @@
+package sg.edu.rp.c346.senderapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSend;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSend = findViewById(R.id.button);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //call the method sendBroadcast() to send this broadcast to all receiver in undefine order
+                Intent intent = new Intent("com.example.broadcast.MY_BROADCAST");
+                sendBroadcast(intent);
+            }
+
+        });
+    }
+}
+
